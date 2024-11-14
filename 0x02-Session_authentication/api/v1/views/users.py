964bebb4b397d545353bc9bfcca3d5a75f1fda88
gomolemontlhane <gomolemontlhane@gmail.com>
2024-11-14 23:04:33 +0200

    adding files

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Users views
-"""
+""" Module of Users views """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
@@ -10,8 +9,7 @@ from models.user import User
 def view_all_users() -> str:
     """ GET /api/v1/users
     Return:
-      - list of all User objects JSON represented
-    """
+      - list of all User objects JSON represented """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
@@ -23,20 +21,20 @@ def view_one_user(user_id: str = None) -> str:
       - User ID
     Return:
       - User object JSON represented
-      - 404 if the User ID doesn't exist
-    """
+      - 404 if the User ID doesn't exist """
+
     if user_id is None:
         abort(404)
-    if user_id == "me":
+
+    if user_id == "me":  # Handle the case where user_id is "me"
         if request.current_user is None:
             abort(404)
-        user = request.current_user
-        return jsonify(user.to_json())
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
-    if request.current_user is None:
-        abort(404)
+
     return jsonify(user.to_json())
 
 
